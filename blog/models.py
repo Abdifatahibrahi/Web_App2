9ecefcc19b3318f3c0ab6cf91ea7b26231a2337d
Abdifatah <alsidiq099@gmail.com>
2022-02-24 18:50:09 +0300

    updated blog posts and added some more functionalities

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -8,3 +9,11 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse("blog-detail", kwargs={"pk": self.pk})
+    
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import (ListView, 
+DetailView, 
+CreateView
+)
 
 
 posts = Post.objects.all()
@@ -11,6 +15,28 @@ def home(request):
         "posts": posts
     }
     return render(request, "blog/home.html", my_context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = "/"
+    
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 
 def about(request):
     return render(request, "blog/about.html")
